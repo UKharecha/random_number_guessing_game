4db810c778e93095f223f0879bb024489e1c4238
Umang <UKharecha>
2020-10-16 13:47:10 +0200

    Add game.py

---
+++ game.py
@@ -0,0 +1,16 @@
+# This is a random number guessing game between computer and the user.
+import random
+
+
+def ask_user():
+    while True:
+        computer_num = str(random.randint(1, 10))
+        user_input = str(input("Please choose a number between 1-10\n"))
+        if user_input == computer_num:
+            print(f"Computer choose {computer_num} and you choose {user_input}. Congratulations! you win")
+            break
+        else:
+            print(f"Computer choose {computer_num} and you choose {user_input}. Try again.")
+
+
+ask_user()
