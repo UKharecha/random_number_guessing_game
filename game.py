755a94929f64d6e5d4d97b4bfa5de797fc8f5d8d
Umang <UKharecha>
2020-10-16 14:34:33 +0200

    Change number range

--- game.py
+++ game.py
@@ -4,8 +4,8 @@ import random
 
 def ask_user():
     while True:
-        computer_num = str(random.randint(1, 10))
-        user_input = str(input("Please choose a number between 1-10\n"))
+        computer_num = str(random.randint(1, 20))
+        user_input = str(input("Please choose a number between 1-20\n"))
         if user_input == computer_num:
             print(f"Computer choose {computer_num} and you choose {user_input}. Congratulations! you win")
             break
